d306ad38b92a6d50493d5a571f773bb3a1f2a3d1
Todd Killick <killicktodd@yahoo.co.uk>
2015-04-29 12:44:02 +0100

    Generate comments controller, include instructions

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,39 @@
+class CommentsController < ApplicationController
+  
+  def new
+  	# find the post
+  	# Assign a new empty comment to @comment
+  end
+
+  def create
+  	# find the post
+  	# Assign a new comment with form data to @comment
+  	# If it saves
+  	# Flash sucess and redirect to show post
+  	# Else
+  	# Flash error and render new
+  end
+
+  def edit
+  	# find the post
+  	# Assign the comment to @comment
+  end
+
+  def update
+  	# find the post
+  	# Assign the comment to @comment
+  	# If it updates with DB data 
+  	# Flash success and redirect to show post
+  	# Else
+  	# Flash error and render 
+  end
+
+  def destroy
+  	# find the post
+  	# Delete comment
+  	# Redirect to show
+  end
+
+  # Whitelist the data from the form
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'comments/new'
+
+  get 'comments/edit'
+
   root "posts#index"
   resources :posts do
   	resources :comments
